337e3588389153d00e5eacf99f68f1b09fd7823c
Abigail Read <AbbyReadMail@gmail.com>
2023-10-09 11:10:32 -0700

    Make it look like it should work

--- src/convert.c
+++ src/convert.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-int check_usage(int argc, char **argv, FILE *in, FILE *out);
-
-struct hexit_pair {
-	unsigned int high : 4;
-	unsigned int low : 4;
-};
-
-struct output_flag {
-	unsigned int tab : 1;
-	unsigned int newline  : 1;
-	unsigned int nibble_counter : 1;
-};
+#include "convert.h"
 
 int main(int argc, char *argv[]) {
 	char ch;
@@ -21,13 +9,10 @@ int main(int argc, char *argv[]) {
 	check_usage(argc, argv, in, out);
 
 	struct output_flag flag;
-	#define NO  0
-	#define YES 1
-	#define HIGH 0
-	#define LOW  1
 	flag.tab = NO;
+	flag.space = NO;
 	flag.newline  = NO;
-	flag.nibble_counter = HIGH;
+	flag.next_nibble = HIGH;
 
 	// values stored in a byte
 	struct hexit_pair nibble;
@@ -35,23 +20,76 @@ int main(int argc, char *argv[]) {
 	nibble.low = 0;
 
 	while ((ch = getc(in)) != EOF) {
-		if (ch )
-		if (flag.nibble_counter == HIGH) {
-			nibble.high = get_tokex(ch, flag);
-		} else {
-			nibble.low  = get_tokex(ch, flag);
-		}
-		if (flag.)
+		// Glean Tokex character from input (0xF means invalid)
+		char tokex_char = get_tokex(ch, flag);
 
-		if (nibble.high != 0xF) {}
-		if (flag.tab == YES) {}
-		if (flag.nl  == YES) {}
+		// Only process character if it is a valid input
+		if (tokex_char != 0xF) {
+			int offset; // for tab rounding
+
+			// If at start of output byte
+			if (flag.next_nibble == HIGH) {
+				if (flag.tab || flag.space || flag.newline) {
+					fputc(0, out); // hex 00 byte
+					if (flag.newline) fputc(0, out); // extra for nl
+					if (flag.tab && !(offset = ftell(out) % TAB_SIZE)) {
+						int bytes_to_write = TAB_SIZE - offset;
+						for (int i = 0; i < bytes_to_write; ++i) {
+							fputc(0, out);
+						}
+					}
+					flag.next_nibble = HIGH;
+				} else { // valid alphabetic character
+					nibble.high = tokex_char;
+					flag.next_nibble = LOW;
+				}
+				// End high-nibble entry
+			} else // to not double-count an input character
+			if (flag.next_nibble == LOW) {
+
+				// Check if we need to pad to bytes
+				if (flag.tab || flag.space || flag.newline) {
+					nibble.low = 0; // padding before actual
+					ch = construct_byte(nibble); // pad it
+					fputc(ch, out); // output padded preceding byte
+					fputc(0, out); // hex 00 actual byte
+					if (flag.newline) fputc(0, out); // extra for newline
+					if (flag.tab && !(offset = ftell(out) % TAB_SIZE)) {
+						int bytes_to_write = TAB_SIZE - offset;
+						for (int i = 0; i < bytes_to_write; ++i) {
+							fputc(0, out);
+						}
+					}
+				} else { // valid alphabetic character
+					nibble.low = tokex_char;
+					ch = construct_byte(nibble);
+					fputc(ch, out);
+				}
+				flag.next_nibble = HIGH;
+			}
+		}
 	}
-	
+	fclose(in);
+	fclose(out);
 	return 0;
 }
 
+// Should only need to be called on next_nibble LOW since you'd either be
+// 	finishing a two character entry or padding an uneven one.
+// Don't need to call it if you're just outputting a 00 byte.
+char construct_byte(struct hexit_pair nibble) {
+	char ch = 0;
+	ch &= nibble.low;
+	ch &= nibble.high << 4;
+	return ch;
+}
+
 char get_tokex(const char ch, struct output_flag flag){
+	// Clear flags in prep
+	flag.tab = NO;
+	flag.space = NO;
+	flag.newline = NO;
+
 	switch(ch) {
 		case 'A':
 		case 'a':
@@ -135,6 +173,7 @@ char get_tokex(const char ch, struct output_flag flag){
 		
 		case ' ':
 		case '.':
+			flag.space = YES;
 			return 0x0;
 		break;
 
@@ -143,22 +182,4 @@ char get_tokex(const char ch, struct output_flag flag){
 			return 0xF;
 		break;	
 	} 
-}
-
-int check_usage(int argc, char **argv, FILE *in, FILE *out) {
-	if (argc != 3) {
-		fprintf(stderr, "Usage: ./%s <in> <out>\n", *argv[0]);
-		exit(1);
-	}
-
-	if ((in = fopen(argv[1], "r")) == NULL) {
-		fprintf(stderr, "Cannot open %s.\n", argv[1]);
-		exit(1);
-	}
-
-	if ((out = fopen(argv[2], "wb")) == NULL) {
-		fprintf(stderr, "Cannot open %s.\n", argv[1]);
-		exit(1);
-	}
-	return 0;
 }
